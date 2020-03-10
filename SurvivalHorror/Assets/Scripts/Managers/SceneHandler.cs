5bd38d7d180e49eca8db22c0d15e113c71ffcca8
bruno takashi <bt.tengan@gmail.com>
2020-03-10 20:27:31 -0300

    Carregamento de cenas
    - cena MainMenu
    - cena GameOver
    - script SceneHandler
    - script GameManager

---
+++ SurvivalHorror/Assets/Scripts/Managers/SceneHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour {
+	//public Animator transition;
+
+	public void LoadScene(string scene) {
+		StartCoroutine(DoSceneLoad(scene));
+	}
+
+	IEnumerator DoSceneLoad(string scene) {
+		//transition.SetTrigger("Start");
+		//yield return new WaitForEndOfFrame();
+		//yield return new WaitForSecondsRealtime(transition.GetCurrentAnimatorStateInfo(0).length);
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+		yield break;
+	}
+}
