ecf93cb03f1d9b7d16dbbece7496a875eca02e5a
bruno takashi <bt.tengan@gmail.com>
2020-03-15 01:51:05 -0300

    Transição de cena básica

--- SurvivalHorror/Assets/Scripts/Managers/SceneHandler.cs
+++ SurvivalHorror/Assets/Scripts/Managers/SceneHandler.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour {
-	//public Animator transition;
+	[SerializeField] GameObject _defaultTransition = null;
+	Animator _animator = null;
+
+	void Awake() {
+		if (_defaultTransition) {
+			GameObject transition = Instantiate(_defaultTransition);
+			_animator = transition.GetComponent<Animator>();
+		}
+	}
 
 	public void LoadScene(string scene) {
 		StartCoroutine(DoSceneLoad(scene));
 	}
 
 	IEnumerator DoSceneLoad(string scene) {
-		//transition.SetTrigger("Start");
-		//yield return new WaitForEndOfFrame();
-		//yield return new WaitForSecondsRealtime(transition.GetCurrentAnimatorStateInfo(0).length);
+		if (_animator) {
+			_animator.SetBool("in", true);
+			yield return new WaitForEndOfFrame();
+			yield return new WaitForSecondsRealtime(_animator.GetCurrentAnimatorStateInfo(0).length);
+		}
 		AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
 		yield break;
 	}
