a41617598aa0fbda029b2cd1fb1f70bfd0972f27
bruno takashi <bt.tengan@gmail.com>
2020-03-16 21:26:24 -0300

    Personagem olha na direção do mouse

---
+++ SurvivalHorror/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+	// The up vector is the one who points towards target.
+	void FixedUpdate() {
+		Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		target.z = 0;
+		transform.LookAt(transform.position + Vector3.forward, target);
+	}
+}
