4fa3e2dadbdcc2469210c70298c8898480f00700
bruno takashi <bt.tengan@gmail.com>
2020-04-02 03:58:51 -0300

    Sistema de vida
    Pode ser utilizado tanto para o player quanto para qualquer coisa que precise ter um contador de vida.

---
+++ SurvivalHorror/Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour {
+	[SerializeField] HealthInfo _healthInfo = null;
+	public int _health = 0;
+	public int _maxHealth = 0;
+
+	public Action<int> healthUpdate;
+
+	void Awake() {
+		// Load all health info.
+		_maxHealth = _healthInfo.maxHealth;
+		_health = _maxHealth;
+	}
+
+	void Start() {
+		// Send initial value.
+		if (healthUpdate != null) {
+			healthUpdate(_health);
+		}
+	}
+
+	public void DealDamage(int damage) {
+		if (damage > 0) {
+			_health -= damage;
+			_health = _health < 0 ? 0 : _health;
+			if (healthUpdate != null) {
+				healthUpdate(_health);
+			}
+		}
+	}
+
+	public void HealDamage(int heal) {
+		if (heal > 0) {
+			_health += heal;
+			_health = _health > _maxHealth ? _maxHealth : _health;
+			if (healthUpdate != null) {
+				healthUpdate(_health);
+			}
+		}
+	}
+
+	/*
+	// Test.
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			DealDamage(5);
+		}
+		if (Input.GetKeyDown(KeyCode.E)) {
+			HealDamage(5);
+		}
+	}
+	*/
+}
---
+++ SurvivalHorror/Assets/Scripts/TextUpdateHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextUpdateHealth : MonoBehaviour {
+	[SerializeField] Health _health = null;
+	TextMeshProUGUI _text = null;
+
+	void Awake() {
+		_text = gameObject.GetComponent<TextMeshProUGUI>();
+		if (_text == null) {
+			Debug.LogWarning("no textmeshpro");
+		}
+
+		_health.healthUpdate += UpdateText;
+	}
+
+	void UpdateText(int health) {
+		_text.text = "HP: " + health;
+	}
+}
