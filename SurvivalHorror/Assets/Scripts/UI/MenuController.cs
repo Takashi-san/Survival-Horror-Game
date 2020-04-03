797b0b417424d99be5e6bb5d342592ddfab9c823
bruno takashi <bt.tengan@gmail.com>
2020-04-02 21:58:43 -0300

    Criado menu de pausa
    - existe um controlador de menus que pode ser utilizado para outras interfaces semelhantes.
    - o menu de pausa envolve mudanças no timescale do tempo do Unity.
    - pego os shaders para o blur na internet.

---
+++ SurvivalHorror/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	[SerializeField] GameObject _menu = null;
+	[SerializeField] KeyCode _input = KeyCode.Escape;
+
+	void Start() {
+		if (!_menu) {
+			Debug.LogWarning("No Menu specified to control");
+		}
+		else {
+			_menu.SetActive(false);
+		}
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(_input)) {
+			if (_menu.activeInHierarchy) {
+				Deactivate();
+			}
+			else {
+				Activate();
+			}
+		}
+	}
+
+	void OnDestroy() {
+		Time.timeScale = 1;
+	}
+
+	public void Activate() {
+		_menu.SetActive(true);
+		Time.timeScale = 0;
+	}
+
+	public void Deactivate() {
+		_menu.SetActive(false);
+		Time.timeScale = 1;
+	}
+}
