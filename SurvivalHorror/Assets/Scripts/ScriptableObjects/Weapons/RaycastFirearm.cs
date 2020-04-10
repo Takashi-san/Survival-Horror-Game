46b072d0ae65ab3275b56533884a9e3776f96b88
bruno takashi <bt.tengan@gmail.com>
2020-04-10 12:56:33 -0300

    Raycast simples

--- SurvivalHorror/Assets/Scripts/ScriptableObjects/Weapons/RaycastFirearm.cs
+++ SurvivalHorror/Assets/Scripts/ScriptableObjects/Weapons/RaycastFirearm.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "RaycastFirearm", menuName = "ScriptableObjects/Firearm/RaycastFirearm")]
 public class RaycastFirearm : Firearm {
 	public override void Fire(GameObject owner, Quaternion direction) {
+		RaycastHit2D target = Physics2D.Raycast(owner.transform.position, direction * Vector3.up);
 
+		if (target.collider != null) {
+			Debug.Log("Hit!");
+			Debug.DrawLine(owner.transform.position, target.point, Color.yellow, 1);
+		}
 	}
 }
