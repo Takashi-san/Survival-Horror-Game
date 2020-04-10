729fabdc154ebb9bb5bcb4314e192c18015cf504
bruno takashi <bt.tengan@gmail.com>
2020-04-09 22:28:32 -0300

    Redução da variação da mira com o tempo

--- SurvivalHorror/Assets/Scripts/Shooter.cs
+++ SurvivalHorror/Assets/Scripts/Shooter.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Shooter : MonoBehaviour {
 	[SerializeField] GameObject _projectile = null;
 	[SerializeField] float _variation = 0;
+	[SerializeField] float _variationReduction = 0;
+	[SerializeField] float _variationIncrease = 0;
 	[SerializeField] float _fireRate = 1;
 	[SerializeField] bool _isHold = true;
 	float _timer = 0;
 	float _deviation = 0;
+
+	void Start() {
+		// setup
+		_deviation = _variation;
+	}
 
 	void Update() {
 		_timer += Time.deltaTime;
@@ -22,26 +29,39 @@ public class Shooter : MonoBehaviour {
 			Debug.DrawRay(transform.position, tmp1, Color.magenta);
 
 			// shrink variation.
+			_deviation -= _variationReduction * Time.deltaTime;
+			_deviation = _deviation < 0 ? 0 : _deviation;
+			tmp1 = Quaternion.Euler(transform.rotation.eulerAngles + (Vector3.forward * _deviation)) * tmp;
+			Debug.DrawRay(transform.position, tmp1, Color.white);
+			tmp1 = Quaternion.Euler(transform.rotation.eulerAngles - (Vector3.forward * _deviation)) * tmp;
+			Debug.DrawRay(transform.position, tmp1, Color.white);
 
 			if (_timer > _fireRate) {
 				if (_isHold) {
 					if (Input.GetMouseButton(0)) {
 						Shoot();
 						_timer = 0;
+						_deviation += _variationIncrease;
+						_deviation = _deviation > _variation ? _variation : _deviation;
 					}
 				}
 				else {
 					if (Input.GetMouseButtonDown(0)) {
 						Shoot();
 						_timer = 0;
+						_deviation += _variationIncrease;
+						_deviation = _deviation > _variation ? _variation : _deviation;
 					}
 				}
 			}
 		}
+		else {
+			_deviation = _variation;
+		}
 	}
 
 	void Shoot() {
-		float variation = Random.Range(-_variation, _variation);
+		float variation = Random.Range(-_deviation, _deviation);
 		Quaternion direction = Quaternion.Euler(transform.rotation.eulerAngles + (Vector3.forward * variation));
 		Instantiate(_projectile, transform.position, direction);
 	}
