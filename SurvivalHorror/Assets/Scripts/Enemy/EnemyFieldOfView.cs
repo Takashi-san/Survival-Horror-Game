b7439f8631a2d13ca8042b587f8fbb2196815462
bruno takashi <bt.tengan@gmail.com>
2020-09-17 00:22:12 -0300

    Simple enemy - see and follow player

---
+++ SurvivalHorror/Assets/Scripts/Enemy/EnemyFieldOfView.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class EnemyFieldOfView : MonoBehaviour {
+	public Action<Vector3> sawPlayer;
+
+	[SerializeField] [Range(0, 360)] float _fov = 0;
+	[SerializeField] [Min(1)] int _rayCount = 1;
+	[SerializeField] [Min(0)] float _viewDistance = 0;
+	[SerializeField] LayerMask _ignore = new LayerMask();
+	[SerializeField] bool _viewFOV = false;
+	Mesh _mesh;
+
+	void Start() {
+		_mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = _mesh;
+	}
+
+	void Update() {
+		Vector3 origin = transform.position;
+		Quaternion direction = transform.rotation;
+		Quaternion deltaAngle = Quaternion.AngleAxis(_fov / 2, Vector3.forward);
+
+		Vector3[] vertices = new Vector3[_rayCount + 2];
+		Vector2[] uv = new Vector2[vertices.Length];
+		int[] triangles = new int[_rayCount * 3];
+		vertices[0] = Vector3.zero;
+
+		bool __sawPlayer = false;
+		Vector3 __sawPosition = Vector3.zero;
+
+		for (int i = 0; i < _rayCount + 1; i++) {
+			RaycastHit2D hit = Physics2D.Raycast(origin, direction * deltaAngle * Vector3.up, _viewDistance, ~_ignore);
+
+			// Avaliate collision.
+			if (hit.collider != null) {
+				if (hit.transform.tag == "Player") {
+					if (sawPlayer != null) {
+						__sawPlayer = true;
+						__sawPosition = hit.transform.position;
+					}
+				}
+			}
+
+			// FOV visualization.
+			if (_viewFOV) {
+				if (hit.collider == null) {
+					vertices[i + 1] = deltaAngle * Vector3.up * _viewDistance;
+				}
+				else {
+					vertices[i + 1] = deltaAngle * Vector3.up * Vector2.Distance(origin, hit.point);
+				}
+
+				if (i > 0) {
+					triangles[(i - 1) * 3] = 0;
+					triangles[(i - 1) * 3 + 1] = i;
+					triangles[(i - 1) * 3 + 2] = i + 1;
+				}
+			}
+
+			deltaAngle *= Quaternion.AngleAxis(-_fov / _rayCount, Vector3.forward);
+		}
+
+		// FOV visualization.
+		if (_viewFOV) {
+			_mesh.vertices = vertices;
+			_mesh.uv = uv;
+			_mesh.triangles = triangles;
+			_mesh.bounds = new Bounds(origin, Vector3.one * _viewDistance * 2);
+		}
+
+		// Notify collision.
+		if (__sawPlayer) {
+			sawPlayer(__sawPosition);
+		}
+	}
+}
---
+++ SurvivalHorror/Assets/Scripts/Enemy/Simple/SimpleEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour {
+	[SerializeField] [Min(0)] float _moveSpeed = 0;
+	[SerializeField] [Min(0)] float _lostPlayerTime = 0;
+	[SerializeField] EnemyFieldOfView _fieldOfView = null;
+	Rigidbody2D _rb2d = null;
+	Vector2 _moveDirection = Vector2.zero;
+	Vector3 _playerLastSawPosition;
+	float _playerLastSawTimer;
+
+	void Awake() {
+		_fieldOfView.sawPlayer += PlayerOnLineOfSight;
+		_rb2d = GetComponent<Rigidbody2D>();
+		_playerLastSawTimer = _lostPlayerTime;
+	}
+
+	void Update() {
+		_moveDirection = Vector2.zero;
+
+		// Avaliate if lost player out of sight.
+		if (_playerLastSawTimer < _lostPlayerTime) {
+			_playerLastSawTimer += Time.deltaTime;
+			_moveDirection = _playerLastSawPosition - transform.position;
+			transform.LookAt(transform.position + Vector3.forward, _moveDirection);
+		}
+	}
+
+	void FixedUpdate() {
+		// Make move.
+		if (_moveDirection != Vector2.zero) {
+			Vector2 target = (Vector2)transform.position + (_moveDirection * _moveSpeed * Time.fixedDeltaTime);
+			_rb2d.MovePosition(target);
+		}
+	}
+
+	void PlayerOnLineOfSight(Vector3 p_sawPosition) {
+		_playerLastSawPosition = p_sawPosition;
+		_playerLastSawTimer = 0;
+	}
+}
